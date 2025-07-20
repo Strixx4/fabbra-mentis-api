cc963b393f90567039690a56a39a2c8edcdfd28c
Strixx <andreasbabo6@gmail.com>
2025-07-20 17:03:38 +0200

    FAM-8: Added Mapper For Items

--- src/FabbraMentis.JsonConverterConsoleApp/Program.cs
+++ src/FabbraMentis.JsonConverterConsoleApp/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using FabbraMentis.JsonConverter.Object;
+using Newtonsoft.Json;
+
+Console.WriteLine("Starting");
+
+try
+{
+    // Item
+    var json = File.ReadAllText(@"C:\Users\andre\Repo\fabbra-mentis-api\src\FabbraMentis.JsonConverterConsoleApp\json\items-base.json");
+    var itemProperty = JsonConvert.DeserializeObject<Item_Property>(json);
+    
+    Console.WriteLine($"Item_Property deserialized successfully: {itemProperty is not null}");
+    Console.WriteLine($"Deserialized {itemProperty?.Baseitem.Count} Base items");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Message: {e.Message}");
+    Console.WriteLine($"StackTrace: {e.StackTrace}");
+}
