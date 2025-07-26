76b4216b81f340b531367417fe2c5c8697a51924
Strixx <andreasbabo6@gmail.com>
2025-07-26 23:41:19 +0200

    FAM-14: Added Object

---
+++ src/FabbraMentis.JsonConverter.Object/Object/ConditionsDiseases.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace FabbraMentis.JsonConverter.Object;
+
+public class ConditionFluff
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonProperty("source")]
+    public string Source { get; set; } = null!;
+
+    [JsonProperty("images")]
+    public List<Image> Images { get; set; } = [];
+}
+
+public class Href
+{
+    [JsonProperty("type")]
+    public string Type { get; set; } = null!;
+
+    [JsonProperty("path")]
+    public string Path { get; set; } = null!;
+}
+
+public class Image
+{
+    [JsonProperty("type")]
+    public string Type { get; set; } = null!;
+
+    [JsonProperty("href")]
+    public Href Href { get; set; } = null!;
+}
+
+public class ConditionsDiseasesRoot
+{
+    [JsonProperty("conditionFluff")]
+    public List<ConditionFluff> ConditionFluff { get; set; } = [];
+}
